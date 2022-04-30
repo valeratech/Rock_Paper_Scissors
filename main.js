a5813ee7f39fb9d42a60ac623ba7aaeed996e9d1
Administrator <valeraryan@gmail.com>
2022-04-30 13:32:35 -0700

    Player function created to select rock, paper, or scissors

--- main.js
+++ main.js
@@ -3,7 +3,17 @@ function computerPlay() {
     let typeList = ['Rock', 'Paper', 'Scissors'];
     // Uses the Math.Random to generate a random int and then use floor to return a whole num
     let random = Math.floor(Math.random() * typeList.length);
-    return typeList[random];
+    return typeList[random].toLowerCase();
 };
 
-console.log(computerPlay());
+function playerPlay() {
+    playerChoice = prompt("Rock, Paper, or Scissors? :").toLowerCase();
+    return playerChoice;
+};
+
+function game(compFunc, playFunc) {
+    console.log(compFunc, playFunc);
+
+};
+
+game(computerPlay(), playerPlay());
